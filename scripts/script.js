db62227b925003c39f585abe0fb3b1aa93f41dda
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-09 15:00:06 +0200

    creato array con oggetti bici e struttura html dove mostrare la bici

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const arrayBici = [
+    {
+        nomeBici: 'BlastBike-400',
+        peso: 14.325
+    },
+    {
+        nomeBici: 'VAN RYSEL EDR',
+        peso: 7.5
+    },
+    {
+        nomeBici: 'TRIBAN RC120',
+        peso: 12.54
+    },
+    {
+        nomeBici: 'CLARIS EDR AF',
+        peso: 10.20
+    },
+    {
+        nomeBici: 'SUZUKI GRVL 500',
+        peso: 3.45
+    },
+    {
+        nomeBici: 'SRAM RIVAL AXS RCR-F PRO 12 E1 2X12V DI4 Levar 2.0 VAN RYSEL',
+        peso: 1.80
+    }
+];
+
