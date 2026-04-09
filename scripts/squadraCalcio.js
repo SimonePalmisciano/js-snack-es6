43429645463f979eb79c834c2928c6c650e370c4
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-09 16:25:47 +0200

    creato array con oggetti delle squadre e un ciclo for che scorre tutti gli oggetti

--- scripts/squadraCalcio.js
+++ scripts/squadraCalcio.js
@@ -1,2 +1,41 @@
 'use strict';
 
+const arraySquadreCalcio = [
+    {
+        nome: 'Barnsley',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Hull City',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Charlton',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Bristol City',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Millwall',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Oxford Utd',
+        puntiFatti: 0,
+        falliSubiti: 0
+    }
+];
+
+// scorrere l'array un oggetto alla volta
+
+for (const squadre of arraySquadreCalcio) {
+    console.log(squadre);
+}
+
