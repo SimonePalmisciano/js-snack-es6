bcd2520597ec45d10122adc583f406ec1fafce17
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-09 17:00:17 +0200

    esercizio completato

--- scripts/squadraCalcio.js
+++ scripts/squadraCalcio.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const arraySquadreCalcio = [
+let arraySquadreCalcio = [
     {
         nome: 'Barnsley',
         puntiFatti: 0,
@@ -34,17 +34,42 @@ const arraySquadreCalcio = [
 ];
 
 // scorrere l'array un oggetto alla volta
-let squadres = [];
+let squadres = []; // ho creato un array di appoggio
 
-for (let z = 0; z < arraySquadreCalcio.length; z++) {
+for (let z = 0; z < arraySquadreCalcio.length; z++) { // creato un ciclo che genera un numero casuale per i punti e falli e lo pusha all'interno del nuovo array
     const element = arraySquadreCalcio[z];
-    const generaNumeriCasualiPunti = Math.floor(Math.random()* 120 + 1);
+    const generaNumeriCasualiPunti = Math.floor(Math.random()* 120 + 1); 
     const generaNumeriCasualiFalli = Math.floor(Math.random()* 50 + 1);
 
     element.puntiFatti = generaNumeriCasualiPunti;
     element.falliSubiti = generaNumeriCasualiFalli;
 
-    squadres.push(element);
+    squadres.push(element); // qui pusha sia il nome rimasto invariato che i punti e falli con numeri casuali
     
 }
-console.log(squadres);
+
+// for (let z = 0; z < arraySquadreCalcio.length; z++) {
+//     const generaNumeriCasualiPunti = Math.floor(Math.random()* 120 + 1);
+//     const generaNumeriCasualiFalli = Math.floor(Math.random()* 50 + 1);
+
+//     arraySquadreCalcio[z].puntiFatti = generaNumeriCasualiPunti;
+//     arraySquadreCalcio[z].falliSubiti = generaNumeriCasualiFalli;
+    
+// }
+
+// array che conterrà solo i nomi e i falli
+const arraySquadreNomiFalli = [];
+
+for (const squadra of squadres) { // ciclo che scorre l'array e crea un nuovo oggeto che contiene il nome e i falli e viene pushato dentro il nuovo array
+
+    const squadraNuova = {
+        nome : squadra.nome,
+        falliSubiti : squadra.falliSubiti
+    };
+
+    arraySquadreNomiFalli.push(squadraNuova)
+    // arraySquadreNomiFalli.push(squadra.nome, squadra.falliSubiti);
+
+}
+
+console.log(arraySquadreNomiFalli);
